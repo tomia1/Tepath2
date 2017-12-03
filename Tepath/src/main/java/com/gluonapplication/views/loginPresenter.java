472626f4339e8c2e9ab5b27c1c443c3e18fda989
jemal <jemal@LAPTOP-9MQ2DLFB.home>
2017-12-03 01:05:43 +0100

    login presenter code

--- Tepath/src/main/java/com/gluonapplication/views/loginPresenter.java
+++ Tepath/src/main/java/com/gluonapplication/views/loginPresenter.java
@@ -1,33 +1,51 @@
 package com.gluonapplication.views;
 
-import java.io.IOException;
-
 import com.gluonhq.charm.glisten.mvc.View;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class loginPresenter {
 
-	public class PrimaryPresenter {
+		
+    private static final ActionEvent event = null;
 
-	    @FXML
-	    private View primary;
+	@FXML
+    private View primary;
 
-	    @FXML
-	    private Button anmelden;
+    @FXML
+    private TextField userName;
 
-	    @FXML
-	    void anmeldenClick(ActionEvent event) throws IOException {
-	    	System.out.println("You are loggin in");
-	    	Parent Home_page_parent = FXMLLoader.load(getClass().getResource("home.fxml"));
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button anmelden;
 
-	    }
+    @FXML
+    void MakeLogin(ActionEvent event) {
+    	String usrName = userName.getText();
+    	String pass = password.getText();
+    	if(usrName.equals("Tepapp") && pass.equals("Tepapp")){
+    		System.out.println("Welcome to TepApp!");
+    	}else
+    		
+    	System.out.println("Wrong password!");
+    	
+    	 
+    	
 
-	}
-
-
+    }
+    public static void maind(String[] args){
+    	loginPresenter pr = new loginPresenter();
+    	pr.MakeLogin(event);;
+    }
 }
+		
+		
+		
+		
+		
+
